e558196fae8301dca6a05e1ba03eb50649f10340
shanto-00-01 <miashanta90@gmail.com>
2026-01-09 20:16:16 +0600

    good practice_01

--- lib/core/constant/text_font_style.dart
+++ lib/core/constant/text_font_style.dart
@@ -3,80 +3,82 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
 class TextFontStyle {
   TextFontStyle._internal();
 
-  static final headline32cFFFFFFInter700 = GoogleFonts.inter(
+  static get headline32cFFFFFFInter700 => GoogleFonts.inter(
     color: AppColors.cWhite,
     fontSize: 32.sp,
     fontWeight: FontWeight.w700,
   );
 
-  static final headline32c04063EInter500 = GoogleFonts.inter(
+  static get headline32c04063EInter500 => GoogleFonts.inter(
     color: AppColors.c04063E,
     fontSize: 32.sp,
     fontWeight: FontWeight.w500,
   );
 
-  static final headline24cFFFFFFInter600 = GoogleFonts.inter(
+  static get headline24cFFFFFFInter600 => GoogleFonts.inter(
     color: AppColors.cWhite,
     fontSize: 24.sp,
     fontWeight: FontWeight.w600,
   );
 
-  static final headline18cFFFFFFInter500 = GoogleFonts.inter(
+  static get headline18cFFFFFFInter500 => GoogleFonts.inter(
     color: AppColors.cWhite,
     fontSize: 18.sp,
     fontWeight: FontWeight.w500,
   );
-  static final text16c04063EInter500 = GoogleFonts.inter(
+  static get text16c04063EInter500 => GoogleFonts.inter(
     color: AppColors.c04063E,
     fontSize: 16.sp,
     fontWeight: FontWeight.w500,
   );
 
 
-  static final text16c5E5E5EInter400 = GoogleFonts.inter(
+  static get text16c5E5E5EInter400 => GoogleFonts.inter(
     color: AppColors.c5E5E5E,
     fontSize: 16.sp,
     fontWeight: FontWeight.w400,
   );
 
-  static final text14c04063EInter500 = GoogleFonts.inter(
+  static get text14c04063EInter500 => GoogleFonts.inter(
     color: AppColors.c04063E,
     fontSize: 14.sp,
     fontWeight: FontWeight.w500,
   );
 
-  static final text14cFFFFFFInter700 = GoogleFonts.inter(
+  static get text14cFFFFFFInter700 => GoogleFonts.inter(
     color: AppColors.cWhite,
     fontSize: 14.sp,
     fontWeight: FontWeight.w700,
   );
-  static final text14c0096FCInter500 = GoogleFonts.inter(
+  static get text14c0096FCInter500 => GoogleFonts.inter(
     color: AppColors.c0096FC,
     fontSize: 14.sp,
     fontWeight: FontWeight.w500,
   );
-  static final text12c5E5E5EInter500 = GoogleFonts.inter(
+  static get text12c5E5E5EInter500 => GoogleFonts.inter(
     color: AppColors.c5E5E5E,
     fontSize: 12.sp,
     fontWeight: FontWeight.w500,
   );
-  static final text12c5E5E5EInter400 = GoogleFonts.inter(
+  static get text12c5E5E5EInter400 => GoogleFonts.inter(
     color: AppColors.c5E5E5E,
     fontSize: 12.sp,
     fontWeight: FontWeight.w400,
   );
-  static final text12c04063EInter400 = GoogleFonts.inter(
+  static get text12c04063EInter400 => GoogleFonts.inter(
     color: AppColors.c04063E,
     fontSize: 12.sp,
     fontWeight: FontWeight.w400,
   );
 
-  static final headline32c000000Inter700 = GoogleFonts.inter(
+  static get headline32c000000Inter700 => GoogleFonts.inter(
     color: AppColors.cBlack,
     fontSize: 32.sp,
     fontWeight: FontWeight.w700,
   );
 }
+
